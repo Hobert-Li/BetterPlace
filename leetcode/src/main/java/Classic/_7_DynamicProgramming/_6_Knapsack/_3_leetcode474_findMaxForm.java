ccc3f0d4332a1e3db1a0985c43db5490e6000f91
FSSDAWN <18280586233@163.com>
2019-09-23 14:08:10 +0800

    日常提交

--- leetcode/src/main/java/Classic/_7_DynamicProgramming/_6_Knapsack/_3_leetcode474_findMaxForm.java
+++ leetcode/src/main/java/Classic/_7_DynamicProgramming/_6_Knapsack/_3_leetcode474_findMaxForm.java
@@ -11,7 +11,36 @@ package Classic._7_DynamicProgramming._6_Knapsack;
 
 public class _3_leetcode474_findMaxForm {
     public int findMaxForm(String[] strs, int m, int n) {
+        if (strs == null || strs.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s : strs) {
+            int ones = 0;
+            int zeros = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+            }
 
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
 
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        _3_leetcode474_findMaxForm solution = new _3_leetcode474_findMaxForm();
+        String[] strs = {"10", "0001", "111001", "1", "0"};
+        int m = 5;
+        int n = 3;
+        System.out.println(solution.findMaxForm(strs, m, n));
     }
 }
---
+++ leetcode/src/main/java/Classic/_7_DynamicProgramming/_6_Knapsack/_4_leetcode322_coinChange.java
@@ -0,0 +1,35 @@
+package Classic._7_DynamicProgramming._6_Knapsack;
+
+import java.util.Arrays;
+
+/**
+ * <p>Description:  xx</p>
+ *
+ * @author 李宏博
+ * @version 1.0
+ * @create 2019/9/18 17:36
+ */
+
+//CYC题解有问题，暂未提交
+public class _4_leetcode322_coinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        _4_leetcode322_coinChange solution = new _4_leetcode322_coinChange();
+        int[] coins = {3,4};
+        int amount = 16;
+        System.out.println(solution.coinChange(coins, amount));
+    }
+}
